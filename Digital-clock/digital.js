8324b23a3504ebe731b8f6c9796924ac96192ffd
thilipraj38 <112260477+thilipraj38@users.noreply.github.com>
2023-06-11 11:33:55 +0530

    Update digital.js

--- Digital-clock/digital.js
+++ Digital-clock/digital.js
@@ -1,1 +1,20 @@
+const time = document.getElementById('time');
+const timeFormat = document.getElementById('timeFormat');
+ 
+document.addEventListener("DOMContentLoaded", () => {
+    setInterval(displayTime, 1000)
+});
+function displayTime() {
+    let date = new Date();
 
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hr = hr < 10 ? `0${hr}` : hr;
+    min = min <10 ? `0${min}` :min;
+    sec = sec < 10 ? `0${sec}` :sec;
+
+    time.innerHTML = `${hr}:${min}:${sec}`;
+    timeFormat.innerHTML = hr>12 ? "PM" : "AM";
+}
